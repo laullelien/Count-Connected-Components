ee17aeeed264c91af4d2495ee294606aab2a0547
Aurel <aurelien.delory@grenoble-inp.org>
2023-04-14 18:42:29 +0200

    ancien  rec corr

--- connectes.py
+++ connectes.py
@@ -19,22 +19,20 @@ def load_instance(filename):
         x_coord = list()
         y_coord = list()
         for l in lines:
-            new_point = [float(f) for f in l.split(",")]
+            new_point=[float(f) for f in l.split(",")]
             x_coord.append(new_point[0])
             y_coord.append(new_point[1])
-        min_x = min(x_coord)
-        max_x = max(x_coord)
-        min_y = min(y_coord)
-        max_y = max(y_coord)
-        cell_size = (0.51*distance)
-        grid = [[[] for _ in range(int((max_y-min_y)/cell_size)+1)]
-                for _ in range(int((max_x-min_x)/cell_size)+1)]
-        visited = [[False for _ in range(int((max_y-min_y)/cell_size)+1)]
-                   for _ in range(int((max_x-min_x)/cell_size)+1)]
-        to_visit = set()
+        min_x=min(x_coord)
+        max_x=max(x_coord)
+        min_y=min(y_coord) 
+        max_y=max(y_coord)
+        cell_size=(0.51*distance)
+        grid=[[ [] for _ in range(int((max_y-min_y)/cell_size)+1) ] for _ in range(int((max_x-min_x)/cell_size)+1)]
+        visited=[[ False for _ in range(int((max_y-min_y)/cell_size)+1) ] for _ in range(int((max_x-min_x)/cell_size)+1)]
+        to_visit=set()
         for i in range(len(x_coord)):
-            idx_x = int((x_coord[i]-min_x)/cell_size)
-            idx_y = int((y_coord[i]-min_y)/cell_size)
+            idx_x=int((x_coord[i]-min_x)/cell_size)
+            idx_y=int((y_coord[i]-min_y)/cell_size)
             grid[idx_x][idx_y].append((x_coord[i], y_coord[i]))
             to_visit.add((idx_x, idx_y))
     return distance**2, grid, to_visit, visited
@@ -47,19 +45,18 @@ def visit(grid, i, j, visited, distance):
     if visited[i][j]:
         return 0
     if not grid[i][j]:
-        visited[i][j] = True
+        visited[i][j]=True
         return 0
-    visited[i][j] = True
-    max_x_idx = len(grid)
-    max_y_idx = len(grid[0])
-    size = len(grid[i][j])
+    visited[i][j]=True
+    max_x_idx=len(grid)
+    max_y_idx=len(grid[0])
+    size=len(grid[i][j])
     for x in range(i-2, i+3):
         for y in range(j-2, j+3):
-            if x >= 0 and y >= 0 and x < max_x_idx and y < max_y_idx and connection(grid, i, j, x, y, distance):
-                size += visit(grid, x, y, visited, distance)
+            if x>=0 and y>=0 and x<max_x_idx and y<max_y_idx and connection(grid, i, j, x, y, distance):
+                size+=visit(grid, x, y, visited, distance)
     return size
-
-
+                
 def connection(grid, i, j, x, y, distance):
     """
     returns whether some points of grid[i][j] and grid[x][y] are connected
@@ -71,18 +68,17 @@ def connection(grid, i, j, x, y, distance):
     return False
 
 
+
 def print_components_sizes(distance, grid, to_visit, visited):
     """
     prints the sizes of the connected components in decreasing order
     """
-    components_sizes = list()
+    components_sizes=list()
     for i, j in to_visit:
         if not visited[i][j]:
             components_sizes.append(visit(grid, i, j, visited, distance))
     components_sizes.sort(reverse=True)
     print(components_sizes)
-    return components_sizes
-
 
 def main():
     """
@@ -90,7 +86,7 @@ def main():
     """
     for instance in argv[1:]:
         distance, grid, to_visit, visited = load_instance(instance)
-        return print_components_sizes(distance, grid, to_visit, visited)
+        print_components_sizes(distance, grid, to_visit, visited)
 
 
 main()
